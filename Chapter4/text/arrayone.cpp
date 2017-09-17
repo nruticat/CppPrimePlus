bbbc618f74be702a130542f7fd351fe820d02d6e
Nruticat <461822039@qq.com>
2017-09-18 00:26:47 +0800

    nruticat

---
+++ Chapter4/text/arrayone.cpp
@@ -0,0 +1,26 @@
+// arrayone.cpp -- small array of integer
+#include <iostream>
+using namespace std;
+int main()
+{
+	int yams[3];
+	yams[0] = 7;
+	yams[1] = 8;
+	yams[2] = 6;
+
+	int yamcosts[3] = { 20,30,5 };
+	cout << "Total yams = ";
+	cout << yams[0] + yams[1] + yams[2] << endl;
+	cout << "The package with " << yams[1] << " yams costs ";
+	cout << yamcosts[1] << " cents per yam.\n";
+	int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
+	total = total + yams[2] * yamcosts[2];
+	cout << "The total yam expense is " << total << " cents.\n";
+
+	cout << "\nSize of yams array = " << sizeof yams;
+	cout << " bytes.\n";
+	cout << "Size of one element = " << sizeof yams[0];
+	cout << " bytes.\n";
+	cin.get();
+	return 0;
+}
